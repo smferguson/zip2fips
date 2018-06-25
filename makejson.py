aeea024fd84303de21073b3520143d3b81f2749e
Scott Ferguson <scott@98ws.net>
2018-06-25 15:33:52 -0400

    simple updates

--- makejson.py
+++ makejson.py
@@ -1,18 +1,24 @@
-#!/usr/bin/env python
-
+import datetime
 import json
 import re
 
+from os import listdir
+
+
+today = datetime.datetime.now().strftime('%Y%m%d')
 statecodes = json.load(open('state_fips.json'))
 zipmap = {}
 
-for i in range(1,11):
-    zfile = open('zipctys/zipcty%d' % i)
-    zfile.readline() # skip first line
-    for l in zfile:
-        m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)       
-        if m:
-            r = m.groupdict()
-            zipmap[r['zip']] = statecodes[r['state']] + r['fips']
+for f in listdir('zipctys'):
+    print(f'processing file {f}')
+    with open(f'zipctys/{f}', 'r') as zfile:
+      zfile.readline() # skip first line
+      for l in zfile:
+          m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)
+          if m:
+              r = m.groupdict()
+              zipmap[r['zip']] = statecodes[r['state']] + r['fips']
 
-print json.dumps(zipmap)
+with open(f'zip_to_fips_{today}.json', 'w') as ztf:
+    ztf.write(json.dumps(zipmap))
+
